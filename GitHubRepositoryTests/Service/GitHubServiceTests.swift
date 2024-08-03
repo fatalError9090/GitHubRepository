be43819ca2bcc44fa0bd76975ce5f56937ccd013
fatalError9090 <141588841+fatalError9090@users.noreply.github.com>
2024-08-03 23:09:05 +0200

    Change GitHubServiceTests with combine. Make NetworkError Equatable.

--- GitHubRepositoryTests/Service/GitHubServiceTests.swift
+++ GitHubRepositoryTests/Service/GitHubServiceTests.swift
@@ -1,101 +1,140 @@
 import XCTest
+import Combine
 @testable import GitHubRepository
 
 class GitHubServiceTests: XCTestCase {
+  
   var sut: GitHubService!
   var mockSession: URLSession!
-
+  var cancellables: Set<AnyCancellable>!
+  
   override func setUp() {
     super.setUp()
-
+    
     let configuration = URLSessionConfiguration.ephemeral
     configuration.protocolClasses = [URLProtocolMock.self]
     mockSession = URLSession(configuration: configuration)
     
     sut = GitHubService(session: mockSession)
+    cancellables = []
   }
-
+  
   override func tearDown() {
     sut = nil
     mockSession = nil
+    cancellables = nil
+    URLProtocolMock.testURLs = [:]
+    URLProtocolMock.response = nil
+    URLProtocolMock.error = nil
     super.tearDown()
   }
-
-  func testFetchRepositoriesSuccess() async throws {
+  
+  func testFetchRepositoriesSuccess() {
     let jsonData = """
         [
             {"id": 1, "name": "Repo1", "description": "Description1"},
             {"id": 2, "name": "Repo2", "description": "Description2"}
         ]
         """.data(using: .utf8)!
-
-    URLProtocolMock.testURLs = [URL(
-      string: "https://api.github.com/users/GitHubRepositoryTest/repos"
-    )!: jsonData]
+    
+    URLProtocolMock.testURLs = [URL(string: "https://api.github.com/users/GitHubRepositoryTest/repos"): jsonData]
     URLProtocolMock.response = HTTPURLResponse(
-      url: URL(
-        string: "https://api.github.com/users/GitHubRepositoryTest/repos"
-      )!,
+      url: URL(string: "https://api.github.com/users/GitHubRepositoryTest/repos")!,
       statusCode: 200,
       httpVersion: nil,
       headerFields: nil
-    )!
-
-    let repositories = try await sut.fetchRepositories(for: "GitHubRepositoryTest")
-
-    XCTAssertEqual(repositories.count, 2)
-    XCTAssertEqual(repositories[0].name, "Repo1")
-    XCTAssertEqual(repositories[1].name, "Repo2")
-  }
-
-  func testFetchRepositoriesInvalidURL() async throws {
-    sut = GitHubService(session: mockSession, baseURL: "invalid_url")
+    )
     
-    do {
-      _ = try await sut.fetchRepositories(for: "GitHubRepositoryTest")
-      XCTFail("Expected to throw, but did not")
-    } catch let error as NetworkError {
-      XCTAssertEqual(error, NetworkError.invalidURL)
-    } catch {
-      XCTFail("Unexpected error: \(error)")
-    }
+    let expectation = self.expectation(description: "Fetch Repositories")
+    
+    sut.repositoriesPublisher
+      .sink(receiveCompletion: { completion in
+        if case let .failure(error) = completion {
+          XCTFail("Unexpected error: \(error)")
+        }
+      }, receiveValue: { repositories in
+        XCTAssertEqual(repositories.count, 2)
+        XCTAssertEqual(repositories[0].name, "Repo1")
+        XCTAssertEqual(repositories[1].name, "Repo2")
+        expectation.fulfill()
+      })
+      .store(in: &cancellables)
+    
+    sut.fetchRepositories(for: "GitHubRepositoryTest")
+    
+    waitForExpectations(timeout: 1, handler: nil)
   }
   
-  func testFetchRepositoriesEmptyUser() async throws {
-    do {
-      _ = try await sut.fetchRepositories(for: "")
-      XCTFail("Expected to throw, but did not")
-    } catch let error as NetworkError {
-      XCTAssertEqual(error, NetworkError.emptyUser)
-    } catch {
-      XCTFail("Unexpected error: \(error)")
-    }
+  func testFetchRepositoriesInvalidURL() {
+    sut = GitHubService(session: mockSession, baseURL: "invalid_url")
+    
+    let expectation = self.expectation(description: "Fetch Repositories")
+    
+    sut.repositoriesPublisher
+      .sink(receiveCompletion: { completion in
+        if case let .failure(error) = completion {
+          XCTAssertEqual(error, NetworkError.invalidURL)
+          expectation.fulfill()
+        }
+      }, receiveValue: { _ in
+        XCTFail("Expected to throw, but did not")
+      })
+      .store(in: &cancellables)
+    
+    sut.fetchRepositories(for: "GitHubRepositoryTest")
+    
+    waitForExpectations(timeout: 1, handler: nil)
   }
-
-  func testFetchRepositoriesDecodingError() async throws {
+  
+  func testFetchRepositoriesEmptyUser() {
+    let expectation = self.expectation(description: "Fetch Repositories")
+    
+    sut.repositoriesPublisher
+      .sink(receiveCompletion: { completion in
+        if case let .failure(error) = completion {
+          XCTAssertEqual(error, NetworkError.emptyUser)
+          expectation.fulfill()
+        }
+      }, receiveValue: { _ in
+        XCTFail("Expected to throw, but did not")
+      })
+      .store(in: &cancellables)
+    
+    sut.fetchRepositories(for: "")
+    
+    waitForExpectations(timeout: 1, handler: nil)
+  }
+  
+  func testFetchRepositoriesDecodingError() {
     let jsonData = """
         [
             {"invalid_field": 1}
         ]
         """.data(using: .utf8)!
     
-    URLProtocolMock.testURLs = [URL(
-      string: "https://api.github.com/users/GitHubRepositoryTest/repos"
-    )!: jsonData]
+    URLProtocolMock.testURLs = [URL(string: "https://api.github.com/users/Apple/repos"): jsonData]
     URLProtocolMock.response = HTTPURLResponse(
-      url: URL(
-        string: "https://api.github.com/users/GitHubRepositoryTest/repos"
-      )!,
+      url: URL(string: "https://api.github.com/users/Apple/repos")!,
       statusCode: 200,
       httpVersion: nil,
       headerFields: nil
-    )!
+    )
     
-    do {
-      _ = try await sut.fetchRepositories(for: "GitHubRepositoryTest")
-      XCTFail("Expected to throw, but did not")
-    } catch let error as NetworkError {
-      XCTAssertEqual(error, NetworkError.decodingFailed)
-    }
+    let expectation = self.expectation(description: "Fetch Repositories")
+    
+    sut.repositoriesPublisher
+      .sink(receiveCompletion: { completion in
+        if case let .failure(error) = completion {
+          XCTAssertEqual(error, NetworkError.decodingFailed)
+          expectation.fulfill()
+        }
+      }, receiveValue: { _ in
+        XCTFail("Expected to throw, but did not")
+      })
+      .store(in: &cancellables)
+    
+    sut.fetchRepositories(for: "GitHubRepositoryTest")
+    
+    waitForExpectations(timeout: 1, handler: nil)
   }
 }
