4d4f493d1e393dbeed419c1dbe97c5833fb8f067
fatalError9090 <141588841+fatalError9090@users.noreply.github.com>
2024-08-03 23:06:59 +0200

    Change GitHubServiceProtocol with combine adding a publisher. Change fetchRepositories(for user: String) method and add publisher in GitHubService with combine.

--- GitHubRepository/API/Service/GitHub/GitHubService.swift
+++ GitHubRepository/API/Service/GitHub/GitHubService.swift
@@ -1,30 +1,49 @@
+import Combine
 import Foundation
 
-class GitHubService: GitHubServiceProtocol {
-
+final class GitHubService: GitHubServiceProtocol {
+  
   private let session: URLSession
   private let baseURL: String
-
+  private let _repositoriesPublisher = PassthroughSubject<[Repository], NetworkError>()
+  private var subscriptions = Set<AnyCancellable>()
+  
+  var repositoriesPublisher: AnyPublisher<[Repository], NetworkError> {
+    _repositoriesPublisher.eraseToAnyPublisher()
+  }
+  
   init(session: URLSession = .shared, baseURL: String = "https://api.github.com/users") {
     self.session = session
     self.baseURL = baseURL
   }
-
-  func fetchRepositories(for user: String) async throws -> [Repository] {
+  
+  func fetchRepositories(for user: String) {
     guard !user.isEmpty else {
-      throw NetworkError.emptyUser
+      _repositoriesPublisher.send(completion: .failure(.emptyUser))
+      return
     }
-
+    
     guard let url = URL(string: "\(baseURL)/\(user)/repos"), url.host != nil else {
-      throw NetworkError.invalidURL
+      _repositoriesPublisher.send(completion: .failure(.invalidURL))
+      return
     }
-
-    let (data, _) = try await session.data(from: url)
-    do {
-      let repositories = try JSONDecoder().decode([Repository].self, from: data)
-      return repositories
-    } catch {
-      throw NetworkError.decodingFailed
-    }
+    
+    session.dataTaskPublisher(for: url)
+      .map(\.data)
+      .decode(type: [Repository].self, decoder: JSONDecoder())
+      .mapError { error in
+        if let _ = error as? DecodingError {
+          return NetworkError.decodingFailed
+        }
+        return NetworkError.unknown(error.localizedDescription)
+      }
+      .sink(receiveCompletion: { completion in
+        if case let .failure(error) = completion {
+          self._repositoriesPublisher.send(completion: .failure(error))
+        }
+      }, receiveValue: { repositories in
+        self._repositoriesPublisher.send(repositories)
+      })
+      .store(in: &subscriptions)
   }
 }
